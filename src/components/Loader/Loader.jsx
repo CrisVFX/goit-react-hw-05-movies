ee19f4395c9d1615a6bfc02b675ad77437a001ff
Cris <132308930+CrisVFX@users.noreply.github.com>
2024-05-03 18:36:35 +0800

    update

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,19 +1,17 @@
-import { RotatingLines } from 'react-loader-spinner';
+import { Vortex } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
 export const Loader = () => {
   return (
     <div className={css.loaderWrap}>
-      <RotatingLines
+      <Vortex
         visible={true}
-        height="96"
-        width="96"
-        color="grey"
-        strokeWidth="5"
-        animationDuration="0.75"
-        ariaLabel="rotating-lines-loading"
+        height="80"
+        width="80"
+        ariaLabel="vortex-loading"
         wrapperStyle={{}}
-        wrapperClass=""
+        wrapperClass="vortex-wrapper"
+        colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
       />
     </div>
   );
